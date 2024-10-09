1cb73a4fc8f183d8f521f798842f0ec4da19dbef
charlottevarlet3wa <c.varlet@3wa.fr>
2024-10-09 10:32:00 +0200

    typo

--- scripts/classes/Question.js
+++ scripts/classes/Question.js
@@ -18,7 +18,7 @@ export default class Question {
     // const response = await fetch("/public/data/questions.json");
     // const response = await fetch("../../public/data/questions.json");
     // const response = await fetch("/questions.json");
-    const response = await fetch("../");
+    const response = await fetch("../questions.json");
     this.questions = await response.json();
   }
 
